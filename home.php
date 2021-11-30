9ba227bc6b0b56711dbaf59bc972f850e1e4473f
brilianaarwaa <sunflaws@gmail.com>
2021-11-30 08:06:42 +0700

    menambahkan file deltrans.php editpel.php edittrans.php dan home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+include('koneksi.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <ol class="breadcrumb">
+        <li>
+          <a href="index.php?page=home"><i class="fa fa-dashboard"></i> Dashboard </a>
+        </li>  
+      </ol>
+    </section>
+  </div>
+  <table width="950px" align="center">
+            <td height="600px" align="center">
+              <h1>MALAS CUCI PAKAIAN???</h1>
+              <img src="assets/images/3.jpg"><br>
+              <h2>BURUAN BAWA KESINI</h2>
+              <p>HANYA DI KELOMPOK 2 LAUNDRY SOLUSINYA</p>
+            </td>
+          </table>
+
+</body>
+</html>
